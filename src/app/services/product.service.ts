632fbc0de5e67fc88a1a6e1d32cfa93886ca272a
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2022-05-31 18:37:47 -0700

    added search feature, dynamic tab features to add tabs to the web page based off the product category.

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,26 +2,49 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Product } from '../common/product';
 import { Observable } from 'rxjs';
-import {map} from 'rxjs/operators'
+import {map} from 'rxjs/operators';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  
+
   private baseUrl = 'http://localhost:8080/api/products';
-  
+  private categoryUrl = 'http://localhost:8080/api/product-category';
+
   constructor(private httpClient: HttpClient) { }
-  
-  getProductList(): Observable<Product[]> {
-    return this.httpClient.get<GetREsponse>(this.baseUrl).pipe(
-      map(response => response._embedded.products)
+
+  getProductList(theCategoryId: number): Observable<Product[]> {
+
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+    return this.getProducts(searchUrl);
+}
+   getProductCategories(): Observable<ProductCategory[]> {
+      return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
     );
   }
-}
+  searchProducts(theKeyWord: string): Observable<Product[]> {
+        const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyWord}`;
+        return this.getProducts(searchUrl);
+    }
 
-interface GetREsponse {
+
+getProducts(searchUrl: string ): Observable<Product[]> {
+return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
+  map(response => response._embedded.products)
+    ); }
+}
+interface GetResponseProducts {
   _embedded: {
     products: Product[];
   };
 }
+
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
+  };
+}
+
